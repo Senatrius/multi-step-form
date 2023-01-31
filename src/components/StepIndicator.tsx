10a08f65f1bdc56cf8d9b7e15d99075232a18116
senatrius <g.makutenas@gmail.com>
2023-01-31 22:31:22 +0200

    update stepper

--- src/components/StepIndicator.tsx
+++ src/components/StepIndicator.tsx
@@ -6,8 +6,10 @@ interface IStep {
 export const StepIndicator = ({ step, current }: IStep) => {
   return (
     <div
-      className={`grid h-8 w-8 place-items-center rounded-full border-2 border-white text-step-number text-primary-400 ${
-        current ? 'bg-neutral text-primary-400' : 'bg-transparent text-white'
+      className={`grid h-8 w-8 place-items-center rounded-full border-2 text-step-number text-primary-400 ${
+        current
+          ? 'border-neutral bg-neutral text-primary-400'
+          : 'border-white bg-transparent text-white'
       }`}>
       {step}
     </div>
--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,21 +1,17 @@
 import { StepIndicator } from './StepIndicator';
 
-export const Stepper = () => {
+export const Stepper = ({ currStep }: { currStep: number }) => {
   return (
     <section className='grid w-full place-items-center bg-[url("./images/bg-sidebar-mobile.svg")] bg-cover bg-no-repeat pt-8 pb-[6.75rem] md:bg-[url("./images/bg-sidebar-desktop.svg")]'>
       <ul className='flex items-center gap-4'>
-        <li>
-          <StepIndicator step={1} />
-        </li>
-        <li>
-          <StepIndicator step={2} />
-        </li>
-        <li>
-          <StepIndicator step={3} />
-        </li>
-        <li>
-          <StepIndicator step={4} />
-        </li>
+        {[1, 2, 3, 4].map(item => (
+          <li key={item}>
+            <StepIndicator
+              step={item}
+              current={currStep === item}
+            />
+          </li>
+        ))}
       </ul>
     </section>
   );
